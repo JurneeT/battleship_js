aeba81645e4e96548f1de58dc844ab4f83109142
meissa10 <meissandoye@hotmail.com>
2020-11-25 00:58:36 +0000

    version 1

---
+++ new_project javascript/cpuBoardInitializer.js
@@ -0,0 +1,36 @@
+//var positionS= require('./cpuBoardInitializer');
+
+/*let CPU = {
+    ships: {
+      battleship: {
+        size: 5,
+        pegs: [null, null, null, null, null],
+        isSunk: false,
+  
+      },
+      cruiser: {
+        size: 4,
+        pegs: [null, null, null, null],
+        isSunk: false,
+  
+      },
+      sub: {
+        size: 3,
+        pegs: [null, null, null],
+        isSunk: false,
+  
+      },
+      destroyer: {
+        size: 2,
+        pegs: [null, null],
+        isSunk: false,
+  
+      },
+    },
+  };
+
+let Board = Array(10).fill().map(() => Array(10).fill(null));
+
+//Board= positionShip(CPU,Board);*/
+
+
